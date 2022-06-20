5ec61a93b009e79622ad35ec727afb63918cbe54
Svenn-Arne Dragly <s@dragly.com>
2022-06-20 12:57:25 +0200

    Use glam in molecular_dynamics
    This uses glam instead of arrays and cgmath. This is due to glam
    implementing Pod and Zeroable, which allows us to reduce the number of
    buffers.

--- visula/src/vertex_attr_format.rs
+++ visula/src/vertex_attr_format.rs
@@ -1,3 +1,5 @@
+use glam::{Vec2, Vec3, Vec4};
+
 pub trait VertexAttrFormat {
     fn vertex_attr_format() -> wgpu::VertexFormat;
 }
@@ -21,6 +23,24 @@ impl VertexAttrFormat for [f32; 3] {
 }
 
 impl VertexAttrFormat for [f32; 4] {
+    fn vertex_attr_format() -> wgpu::VertexFormat {
+        wgpu::VertexFormat::Float32x4
+    }
+}
+
+impl VertexAttrFormat for Vec2 {
+    fn vertex_attr_format() -> wgpu::VertexFormat {
+        wgpu::VertexFormat::Float32x2
+    }
+}
+
+impl VertexAttrFormat for Vec3 {
+    fn vertex_attr_format() -> wgpu::VertexFormat {
+        wgpu::VertexFormat::Float32x3
+    }
+}
+
+impl VertexAttrFormat for Vec4 {
     fn vertex_attr_format() -> wgpu::VertexFormat {
         wgpu::VertexFormat::Float32x4
     }
