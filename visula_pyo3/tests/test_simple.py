4475befa0d6c4b4c7ea2c4e92da4c05b3481d122
Svenn-Arne Dragly <s@dragly.com>
2023-11-04 16:29:27 +0100

    Make function linear in z

--- visula_pyo3/tests/test_simple.py
+++ visula_pyo3/tests/test_simple.py
@@ -25,32 +25,32 @@ class Parameters(Uniform):
 
 
 parameters = Parameters(
-    a=10.0,
-    b=100.0,
-    c=50.0,
+    a=0.0,
+    b=0.0,
+    c=0.0,
 )
 
 parameters_uniform = parameters.instance()
 parameters.update()
 
+
 def create_particles(t, a, b, c):
     a = 10.0 * vl.cos(a)
     b = 100.0 * vl.sin(b)
     c = 50.0 * vl.cos(b)
-    d = 8000
-    x = vl.cos(a * t) + vl.cos(b * t) / 2.0 + vl.sin(c * t) / 3.0 + vl.cos(d * t) / 20.0
-    y = vl.sin(a * t) + vl.sin(b * t) / 2.0 + vl.cos(c * t) / 3.0 + vl.sin(d * t) / 20.0
-    z = 2.0 * vl.cos(t) + vl.cos(2 * d * t) / 20.0 + vl.sin(2 * d * t) / 20.0
+    x = vl.cos(a * t) + vl.cos(b * t) / 2.0 + vl.sin(c * t) / 3.0
+    y = vl.sin(a * t) + vl.sin(b * t) / 2.0 + vl.cos(c * t) / 3.0
+    z = t
     positions = 10.0 * vl.vec3(x, y, z)
     return positions
 
 
-t = InstanceBuffer(np.linspace(0, 2 * 3.14, count))
+t = InstanceBuffer(np.linspace(0, 3.14 * count / 10000, count))
 position = create_particles(t, parameters_uniform.a, parameters_uniform.b, parameters_uniform.c)
 
 spheres = SphereDelegate(
     position=position,
-    radius=0.01,
+    radius=0.04,
     color=1.0 * position / 4.0 + 8.0 / 3.0,
 )
 
@@ -61,8 +61,8 @@ def update():
     global a
     global b
     global c
-    parameters.a += 0.0001
-    parameters.b += 0.00001
+    parameters.a += 0.00001
+    parameters.b += 0.000001
     parameters.c += 0.00001
     parameters.update()
 
