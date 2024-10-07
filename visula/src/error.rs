626f9964c273e64ea0c78e8a7d9f0f01c14fc014
Svenn-Arne Dragly <s@dragly.com>
2024-10-07 09:53:03 +0200

    Derive std::error::Error for visula::Error

--- visula/src/error.rs
+++ visula/src/error.rs
@@ -1,2 +1,10 @@
 #[derive(Clone, Debug)]
 pub struct Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Visula error")
+    }
+}
+
+impl std::error::Error for Error {}
